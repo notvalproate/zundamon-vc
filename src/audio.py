cc031bc8c958777f6458e966ab666af2bf24d82e
Ian Tauro <sushruth127@gmail.com>
2024-06-21 10:06:01 +0530

    Made recorder class instead

--- src/audio.py
+++ src/audio.py
@@ -1,21 +1,31 @@
 import pyaudio
 
-pa = pyaudio.PyAudio()
 CHANNELS = 1
 FRAME_RATE = 16000
 RECORD_SECONDS = 5
 AUDIO_FORMAT = pyaudio.paInt16
 SAMPLE_SIZE = 2
 
-def list_devices():
-    for i in range(pa.get_device_count()):
-        device = pa.get_device_info_by_index(i)
+class Recorder:
+    def __init__(self):
+        self.pa = pyaudio.PyAudio()
+    
+    def list_devices(self):
+        for i in range(self.pa.get_device_count()):
+            device = self.pa.get_device_info_by_index(i)
 
-        if device['maxInputChannels'] > 0:
-            print(f"\nInput Device {i}: {device['name']}\nSample Rate: {device['defaultSampleRate']}")
+            if device['maxInputChannels'] > 0:
+                print(f"\nInput Device {i}: {device['name']}\nSample Rate: {device['defaultSampleRate']}")
 
+    def record_microphone(self, index, chunk):
+        stream = self.pa.open(format=AUDIO_FORMAT, 
+                        channels=CHANNELS, 
+                        rate=FRAME_RATE, 
+                        input=True,
+                        input_device_index=index,
+                        frames_per_buffer=chunk)
+        
+        frames = []
 
-
-
-def terminate_audio():
-    pa.terminate()
+    def __del__(self):
+        self.pa.terminate()
